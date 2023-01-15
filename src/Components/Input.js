b1078b4926f2308a280f27cdbe4b1e4e9b2abb30
ajayjha1 <ajayjha1886@gmail.com>
2023-01-15 20:54:07 +0530

    code files

---
+++ src/Components/Input.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import data from './data';
+
+function Input() {
+    const [noOfTicket, setNoOfTicket] = React.useState("");
+    const [message, setMessage] = React.useState("");
+    var seatBooked = [] // An array to store the seat numbers booked for the user
+
+  // Function to check if all the seats are available in a single row or not
+  const checkRow = () => {
+    var bool = false;
+    if (noOfTicket.target.value <= 7) {
+      // This loop will check if all the seats are available in asingle row
+      for (var i = 0; i <= 12; i++) {
+        var a = 0;
+        for (var j = i*7; j < (i + 1) * 7 && j <=80; j++) {
+          if(data[j] != null ){
+          if (data[j].booked == null || data[j].booked == false) {
+            a++;
+            if (a >= parseInt(noOfTicket.target.value)) {
+              bool = true;
+              bookTicket(i);
+              console.log(data);
+              return;
+            }
+          }
+        }
+        }
+      }
+      if (bool == false) { // If seats are not found in a single row this if statement will be executed
+        var blankSeats = 0;
+        // This function will check if the seats are available or not
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].booked == null || data[i].booked == false) {
+            blankSeats++;
+          }
+          if (blankSeats === parseInt(noOfTicket.target.value)) {
+            bookTicketNearby();
+            bool = true;
+            return;
+          }
+        }
+        if(bool == false){
+          setMessage("Only " + blankSeats+ " seats available")
+        }
+      }
+    } else{
+        // if the user enters a number more than 7 an error message will be displayed
+        setMessage("Enter value less than 7")
+    }
+  };
+
+  const bookTicketNearby = () => {
+    seatBooked = [];
+    var bool = false;
+    var allotedSeats = 0;
+    var m = 2
+    var a = 0
+    while(m<=13){
+      for (var j = 0; j <= m*7 - 1 ; j++) {
+        if(data[j] != null){
+        if(data[j].booked == null || data[j].booked == false){
+          a++
+          if(a == parseInt(noOfTicket.target.value)){
+            for(var k = 0; k <= m*7 -1 ; k++){
+              if(data[k].booked == false || data[k].booked == null){
+              data[k].booked = true;
+              allotedSeats++;
+              
+              seatBooked.push(k+1);
+              }
+              if(allotedSeats == parseInt(noOfTicket.target.value)){
+                bool = true;
+                setMessage("Your seats are " + seatBooked)
+                return;
+              } else{
+                m++;
+                continue;
+              }
+
+            }
+            if(bool == true){
+                setMessage("Your seats are " + seatBooked)
+                return;
+              }
+          }
+        }
+      }
+      }
+      m++;
+    }
+  };
+
+  const bookTicket = (i) => {
+    seatBooked = [];
+    var k = i * 7;
+    var n = parseInt(noOfTicket.target.value) + k;
+    for (var j = k; j < n; j++) {
+      var i = 0;
+      if (data[j].booked == null || data[j].booked == false) {
+        data[j].booked = true;
+        var arr = [];
+        arr[i] = j+1;
+        i++;
+        seatBooked.push(j+1)
+      }
+    }
+    setMessage("Your seats are " + seatBooked)
+  };
+  console.log(data);
+  
+  return (
+    <div style={{ backgroundColor: "lightblue", height:'100%', width: '100%'}} className="App">
+      <h2>Ticket booking app</h2>
+      <h3>Enter no. of tickets you want to book</h3>
+      <input type={"number"} onChange={(e) => setNoOfTicket(e)} />
+      <button onClick={checkRow}>Submit</button>
+      <br />
+      <h6>{message}</h6>
+    </div>
+  )
+}
+
+export default Input;
---
+++ src/Components/data.js
@@ -0,0 +1,324 @@
+const data = [
+    {
+        seatNo: 1,
+        booked: null
+    },
+    {
+        seatNo: 2,
+        booked: null
+    },
+    {
+        seatNo: 3,
+        booked: null
+    },
+    {
+        seatNo: 4,
+        booked: null
+    },
+    {
+        seatNo: 5,
+        booked: null
+    },
+    {
+        seatNo: 6,
+        booked: null
+    },
+    {
+        seatNo: 7,
+        booked: null
+    },
+    {
+        seatNo: 8,
+        booked: null
+    },
+    {
+        seatNo: 9,
+        booked: null
+    },
+    {
+        seatNo: 10,
+        booked: null
+    },
+    {
+        seatNo: 11,
+        booked: null
+    },
+    {
+        seatNo: 12,
+        booked: null
+    },
+    {
+        seatNo: 13,
+        booked: null
+    },
+    {
+        seatNo: 14,
+        booked: null
+    },
+    {
+        seatNo: 15,
+        booked: null
+    },
+    {
+        seatNo: 16,
+        booked: null
+    },
+    {
+        seatNo: 17,
+        booked: null
+    },
+    {
+        seatNo: 18,
+        booked: null
+    },
+    {
+        seatNo: 19,
+        booked: null
+    },
+    {
+        seatNo: 20,
+        booked: null
+    },
+    {
+        seatNo: 21,
+        booked: null
+    },
+    {
+        seatNo: 22,
+        booked: null
+    },
+    {
+        seatNo: 23,
+        booked: null
+    },
+    {
+        seatNo: 24,
+        booked: null
+    },
+    {
+        seatNo: 25,
+        booked: null
+    },
+    {
+        seatNo: 26,
+        booked: null
+    },
+    {
+        seatNo: 27,
+        booked: null
+    },
+    {
+        seatNo: 28,
+        booked: null
+    },
+    {
+        seatNo: 29,
+        booked: null
+    },
+    {
+        seatNo: 30,
+        booked: null
+    },
+    {
+        seatNo: 31,
+        booked: null
+    },
+    {
+        seatNo: 32,
+        booked: null
+    },
+    {
+        seatNo: 33,
+        booked: null
+    },
+    {
+        seatNo: 34,
+        booked: null
+    },
+    {
+        seatNo: 35,
+        booked: null
+    },
+    {
+        seatNo: 36,
+        booked: null
+    },
+    {
+        seatNo: 37,
+        booked: null
+    },
+    {
+        seatNo: 38,
+        booked: null
+    },
+    {
+        seatNo: 39,
+        booked: null
+    },
+    {
+        seatNo: 40,
+        booked: null
+    },
+    {
+        seatNo: 41,
+        booked: null
+    },
+    {
+        seatNo: 42,
+        booked: null
+    },
+    {
+        seatNo: 43,
+        booked: null
+    },
+    {
+        seatNo: 44,
+        booked: null
+    },
+    {
+        seatNo: 45,
+        booked: null
+    },
+    {
+        seatNo: 46,
+        booked: null
+    },
+    {
+        seatNo: 47,
+        booked: null
+    },
+    {
+        seatNo: 48,
+        booked: null
+    },
+    {
+        seatNo: 49,
+        booked: null
+    },
+    {
+        seatNo: 50,
+        booked: null
+    },
+    {
+        seatNo: 51,
+        booked: null
+    },
+    {
+        seatNo: 52,
+        booked: null
+    },
+    {
+        seatNo: 53,
+        booked: null
+    },
+    {
+        seatNo: 54,
+        booked: null
+    },
+    {
+        seatNo: 55,
+        booked: null
+    },
+    {
+        seatNo: 56,
+        booked: null
+    },
+    {
+        seatNo: 57,
+        booked: null
+    },
+    {
+        seatNo: 58,
+        booked: null
+    },
+    {
+        seatNo: 59,
+        booked: null
+    },
+    {
+        seatNo: 60,
+        booked: null
+    },
+    {
+        seatNo: 61,
+        booked: null
+    },
+    {
+        seatNo: 62,
+        booked: null
+    },
+    {
+        seatNo: 63,
+        booked: null
+    },
+    {
+        seatNo: 64,
+        booked: null
+    },
+    {
+        seatNo: 65,
+        booked: null
+    },
+    {
+        seatNo: 66,
+        booked: null
+    },
+    {
+        seatNo: 67,
+        booked: null
+    },
+    {
+        seatNo: 68,
+        booked: null
+    },
+    {
+        seatNo: 69,
+        booked: null
+    },
+    {
+        seatNo: 70,
+        booked: null
+    },
+    {
+        seatNo: 71,
+        booked: null
+    },
+    {
+        seatNo: 72,
+        booked: null
+    },
+    {
+        seatNo: 73,
+        booked: null
+    },
+    {
+        seatNo: 74,
+        booked: null
+    },
+    {
+        seatNo: 75,
+        booked: null
+    },
+    {
+        seatNo: 76,
+        booked: null
+    },
+    {
+        seatNo: 77,
+        booked: null
+    },
+    {
+        seatNo: 78,
+        booked: null
+    },
+    {
+        seatNo: 79,
+        booked: null
+    },
+    {
+        seatNo: 80,
+        booked: null
+    }
+]
+
+export default data;
